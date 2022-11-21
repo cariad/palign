72f8ab2565c821d85dbbb65a008782b60d5e6688
Cariad Eccleston <cariad@cariad.earth>
2022-11-21 17:00:10 +0000

    Add pillow_coords()

---
+++ tests/test_character.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-function-docstring
+
+from palign import Character
+
+
+def test_pillow_coords() -> None:
+    assert Character(character="", x=7, y=9).pillow_coords(3, 3) == (10, 12)
