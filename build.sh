536fd3d7eac6974a29db78058c0c8516b0a92d4b
Cariad Eccleston <cariad@cariad.earth>
2022-11-21 14:58:27 +0000

    Pass tag name to build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,7 @@
 
 set -euo pipefail
 
-if [[ -n ${1:-} ]]; then
-  version=${1}
-elif [[ -n ${GITHUB_REF_NAME:-} ]]; then
-  version=${GITHUB_REF_NAME}
-else
-  version="0.0.0"
-fi
-
-echo "${version}" > palign/VERSION
+echo "${1:?}" > palign/VERSION
 
 rm -rf dist
 python setup.py bdist_wheel
